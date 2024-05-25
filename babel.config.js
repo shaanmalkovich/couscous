732228466a6a704182438d1cf4a524f702f4b8ae
shaanmalkovich <smk4577@gmail.com>
2024-05-25 01:50:20 -0400

    dependencies adjusted

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
-{
-  "presets"; [
-    "@babel/preset-env",
-    "@babel/preset-react",
-    "@babel/preset-typescript"
-  ]
+module.exports = {
+  presets: [
+      ['@babel/preset-env', {targets: {node: 'current'}}],
+  '@babel/preset-typescript',
+],
 }
